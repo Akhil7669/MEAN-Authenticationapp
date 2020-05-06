0122ba47333bafcf7b6725c8baf5c4532cc9c9e5
Akhil7669 <akhilgolla569@gmail.com>
2020-05-06 14:45:27 -0300

    My first commit

---
+++ example.js
@@ -0,0 +1,13 @@
+function greet(callback) {
+    console.log("what is call back function?\n");
+    var obj = {
+        name: 'run my code',
+        name2: 'to know what it mean' 
+    }
+    callback(obj);
+}
+greet(function(obj){
+    console.log(obj.name ,'', obj.name2 ,'\n');
+    console.log("Ans: A function passed to some other function, will be invoked ar some point");
+    
+});
